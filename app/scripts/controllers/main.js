47406dda962bb57175a2f50232e28c6c2bc517fb
satish patil <satish patil>
2016-04-27 10:21:57 +0530

    todo app working on

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item1', 'Item2', 'Item3', 'Item4'];
+    $scope.addTodo = function(){
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    };
+    $scope.removeTodo = function(index){
+    	$scope.todos.splice(index, 1);
+    };
   });
